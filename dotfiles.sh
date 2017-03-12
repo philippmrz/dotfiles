c511aabfbc83c7ddf33c9a5ffdf76b47f561406f
Philipp Merz <phmrch@gmail.com>
2017-03-12 19:52:55 +0100

    Added dotfiles.sh

---
+++ dotfiles.sh
@@ -0,0 +1,11 @@
+mkdir Dotfiles
+cd Dotfiles
+git init
+git pull https://github.com/phmrz/dotfiles.git
+rm -rf ~/.bashrc ~/.config/i3 ~/.vim 
+ln -s ~/Dotfiles/Bash/bashrc ~/.bashrc
+ln -s ~/Dotfiles/Vim/vim ~/.vim
+ln -s ~/Dotfiles/Vim/vimrc ~/.vimrc
+ln -s ~/Dotfiles/X/Xresources ~/.Xresources
+ln -s ~/Dotfiles/i3 ~/.config/i3
+ln -s ~/Dotfiles/i3status ~/.config/i3status
