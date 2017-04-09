3c5d6b6744d40d85eb16e15ad264a3de9dc1917f
Philipp Merz <phmrch@gmail.com>
2017-04-09 14:21:48 +0200

    Added polybar again

---
+++ dotfiles.sh
@@ -0,0 +1,7 @@
+rm -rf ~/.bashrc ~/.config/i3 ~/.vim 
+ln -s ~/Dotfiles/Bash/bashrc ~/.bashrc
+ln -s ~/Dotfiles/Vim/vim ~/.vim
+ln -s ~/Dotfiles/Vim/vimrc ~/.vimrc
+ln -s ~/Dotfiles/X/Xresources ~/.Xresources
+ln -s ~/Dotfiles/i3 ~/.config/i3
+ln -s ~/Dotfiles/i3status ~/.config/i3status
