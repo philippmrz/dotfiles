9735759921b7580af791f9cb1f920c2d82c904e1
Philipp Merz <phmrch@gmail.com>
2017-04-10 21:13:27 +0200

    Fixed i3config for polybar and updated script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-mkdir Dotfiles
-cd Dotfiles
-git init
-git pull https://github.com/phmrz/dotfiles.git
->>>>>>> 089746bf99f6a363f679dbc39e27b1cde320f3a4
 rm -rf ~/.bashrc ~/.config/i3 ~/.vim 
 ln -s ~/Dotfiles/Bash/bashrc ~/.bashrc
 ln -s ~/Dotfiles/Vim/vim ~/.vim
@@ -12,3 +5,7 @@ ln -s ~/Dotfiles/Vim/vimrc ~/.vimrc
 ln -s ~/Dotfiles/X/Xresources ~/.Xresources
 ln -s ~/Dotfiles/i3 ~/.config/i3
 ln -s ~/Dotfiles/i3status ~/.config/i3status
+mkdir ~/.config/polybar
+ln -s ~/Dotfiles/polybar/config ~/.config/polybar/
+mkdir ~/.config/dunst
+ln -s ~/Dotfiles/dunst/dunstrc ~/.config/dunst/
