2c5f7f8ed7d7df9eacc972c3478ca2a210643304
Philipp Merz <phmrch@gmail.com>
2018-01-19 21:51:54 +0100

    Added script to download ibm plex mono and changed dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,66 +1,52 @@
 #!/bin/bash
-echo 'This script takes your dotfile repo and symlinks the important stuff into your home directory.'
-echo 'It also installs programs like git, i3, dunst, compton, vim etc.'
-echo 'Feel free to clone and tweak for your own needs'
-if [ "command -v sudo" = '' ]
-then
-  echo 'Sudo not installed, install to continue'
-  exit
+#Check for root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
 fi
 
-echo 'Installing git'
-apt install git
-echo 'Enter username and email for git config:'
-echo 'user.name: ' && read gitname
-"git config --global user.name '$gitname'"
-echo 'user.email: ' && read gitemail
-"git config --global user.email '$gitemail'"
-cd ~
-echo 'Enter repo user and dotfile name'
-echo 'Repo user: ' && read repouser
-echo 'Repo name: ' && read reponame
-git clone https://www.github.com/$repouser/$reponame
-
-echo 'Tap enter for the programs you want to install'
-echo 'i3WM: ' && read i3
-echo $i3
-if [ "$i3" = 'y' ]
-then 
-  install='i3'
-  echo "Added i3wm"
+#Pipe sudo dpkg status to /dev/null
+dpkg -s sudo 2>/dev/null
+#Use previous command to check if sudo is installed
+if [  $? -ne 0 ]; then
+   echo "Sudo not installed, want to install sudo? (y/n)"
+   read
+   if [ "$REPLY" = "y" ]; then
+      apt install sudo
+   fi
 fi
 
-echo 'dunst: ' && read dunst
-if [ "$dunst" = 'y' ]
-then
-  install="$install dunst"
-  echo 'Added dunst'
+#Install various apps
+echo "Install vim? (y/n)"
+if [ "$REPLY" = "y" ]; then
+   apt install vim
 fi
 
-echo 'compton, feh & rofi: ' && read i3otherstuff
-if [ "$i3otherstuff" = 'y' ]
-then
-  install="$install compton feh rofi"
-  echo 'Added compton, rofi and feh'
-fi
-
-echo "You want to install: $install"
+echo "Install feh? (y/n)"
 read
-
-if [ $EUID -ne 0]
-then
-  echo 'Not root, run as root to continue'
-  exit
+if [ "$REPLY" = "y" ]; then
+   apt install feh
 fi
 
-apt install vim $install
+echo "Install rofi? (y/n)"
+if [ "$REPLY" = "y" ]; then
+   apt install rofi 
+fi
 
+echo "Install compton? (y/n)"
+read
+if [ "$REPLY" = "y" ]; then
+   apt install compton 
+fi
 
-rm -rf ~/.bashrc ~/.config/i3 ~/.vim 
-ln -s ~/Dotfiles/Bash/bashrc ~/.bashrc
-ln -s ~/Dotfiles/Vim/vim ~/.vim
-ln -s ~/Dotfiles/Vim/vimrc ~/.vimrc
-ln -s ~/Dotfiles/X/Xresources ~/.Xresources
-ln -s ~/Dotfiles/i3 ~/.config/i3
-mkdir ~/.config/dunst
-ln -s ~/Dotfiles/dunst/dunstrc ~/.config/dunst/
+echo "Install rxvt? (y/n)"
+read
+if [ "$REPLY" = "y" ]; then
+   apt install rxvt-unicode-256color 
+fi
+
+echo "Install git? (y/n)"
+read
+if [ "$REPLY" = "y" ]; then
+   apt install git
+fi
